2d524282ab4a550b7569216bd3d1e58fc71376bb
Hossein <hossein.zare9978@gmail.com>
2021-03-06 20:18:45 +0330

    Added code to read MNIST

---
+++ read_MNIST.py
@@ -0,0 +1,45 @@
+import numpy as np
+
+# Reading The Train Set
+train_images_file = open('train-images.idx3-ubyte', 'rb')
+train_images_file.seek(4)
+num_of_train_images = int.from_bytes(train_images_file.read(4), 'big')
+train_images_file.seek(16)
+
+train_labels_file = open('train-labels.idx1-ubyte', 'rb')
+train_labels_file.seek(8)
+
+train_set = []
+for n in range(num_of_train_images):
+    image = np.zeros((784, 1))
+    for i in range(784):
+        image[i, 0] = int.from_bytes(train_images_file.read(1), 'big') / 256
+    
+    label_value = int.from_bytes(train_labels_file.read(1), 'big')
+    label = np.zeros((10, 1))
+    label[label_value, 0] = 1
+    
+    train_set.append((image, label))
+
+
+# Reading The Test Set
+test_images_file = open('t10k-images.idx3-ubyte', 'rb')
+test_images_file.seek(4)
+
+test_labels_file = open('t10k-labels.idx1-ubyte', 'rb')
+test_labels_file.seek(8)
+
+num_of_test_images = int.from_bytes(test_images_file.read(4), 'big')
+test_images_file.seek(16)
+
+test_set = []
+for n in range(num_of_test_images):
+    image = np.zeros((784, 1))
+    for i in range(784):
+        image[i] = int.from_bytes(test_images_file.read(1), 'big') / 256
+    
+    label_value = int.from_bytes(test_labels_file.read(1), 'big')
+    label = np.zeros((10, 1))
+    label[label_value, 0] = 1
+    
+    test_set.append((image, label))
